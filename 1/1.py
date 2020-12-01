54a5ee863131af38670820cc4650f5dcc9d83ae6
admin <nicolas.gagnon@prestacap.com>
2020-12-01 09:46:21 +0100

    solution to day 1

--- 1/1.py
+++ 1/1.py
@@ -4,17 +4,32 @@ from pathlib import Path
 def get_data():
     current_dir = Path(__file__).parent
     input_dir = current_dir.joinpath('1.in')
-    data = open(input_dir).read().split(',')
+    data = [int(i) for i in open(input_dir).read().split('\n')]
     return data
 
 
-def do_stuff(data):
-    pass
+def mult_two_numbers(data):
+    addition_mapping = {}
+    for i in data:
+        addition_mapping[2020-i] = i
+    for i in data:
+        if i in addition_mapping:
+            return i * addition_mapping[i]
+    return -1
 
+def mult_three_numbers(data):
+    addition_mapping = {}
+    for i in data:
+        addition_mapping[2020-i] = i
+    for i in data:
+        for j in data[1:]:
+            if i + j in addition_mapping:
+                return i * j * addition_mapping[i+j]
+    return -1
 
 def main():
     data = get_data()
-    result = do_stuff(data)
+    result = mult_three_numbers(data)
     return result
 
 
